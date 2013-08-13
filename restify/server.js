c1f7a6b4df6d690c091a313825dc0cb029cba72e
Patrick Fitzgerald <auskast03@gmail.com>
2013-08-12 18:17:18 -0700

    finish sample restify server

--- restify/server.js
+++ restify/server.js
@@ -6,6 +6,8 @@ var server = restify.createServer({
 
 server.pre(restify.pre.userAgentConnection());
 server.pre(restify.pre.sanitizePath());
+server.use(restify.CORS());
+server.use(restify.fullResponse());
 server.use(restify.acceptParser(server.acceptable));
 server.use(restify.dateParser());
 server.use(restify.authorizationParser());
@@ -16,20 +18,20 @@ server.use(restify.bodyParser());
 // Buyer
 var buyers = {};
 
-function createBuyer(request, response, next) {
+function createBuyer(req, res, next) {
     console.log('create buyer');
-    var buyer = request.body;
+    var buyer = req.body;
 
     buyer.id = Math.random().toString(36).substr(3, 8);
     buyers[buyer.id] = buyer;
-    response.json(201, buyer);
+    res.json(201, buyer);
     return next();
 }
 
-function updateBuyer(request, response, next) {
+function updateBuyer(req, res, next) {
     console.log('update buyer');
-    var id = request.params.id,
-        buyer = request.body;
+    var id = req.params.id,
+        buyer = req.body;
 
     if (!id) {
         return next(new restify.MissingParameterError('Must provide an id.'));
@@ -38,17 +40,17 @@ function updateBuyer(request, response, next) {
     }
 
     buyers[id] = buyer;
-    response.json(202, buyer);
+    res.json(202, buyer);
     return next();
 }
 
-function getBuyer(request, response, next) {
+function getBuyer(req, res, next) {
     console.log('get buyer');
-    var id = request.params.id;
+    var id = req.params.id;
 
     if (id) {
         if (buyers[id]) {
-            response.json(200, buyers[id]);
+            res.json(200, buyers[id]);
             return next();
         } else {
             return next(new restify.NotFoundError('That buyer does not exist.'));
@@ -59,13 +61,13 @@ function getBuyer(request, response, next) {
     for (buyer in buyers) {
         result.push(buyers[buyer]);
     }
-    response.json(result);
+    res.json(result);
     return next();
 }
 
-function deleteBuyer(request, response, next) {
+function deleteBuyer(req, res, next) {
     console.log('delete buyer');
-    var id = request.params.id;
+    var id = req.params.id;
 
     if (!id) {
         return next(new restify.MissingParameterError('Must provide an id.'));
@@ -80,7 +82,7 @@ function deleteBuyer(request, response, next) {
         }
     }
     buyers = result;
-    response.send(204);
+    res.send(204);
     return next();
 }
 
@@ -91,7 +93,7 @@ server.head('/buyer/services/buyers', getBuyer);
 server.get('/buyer/services/buyers/:id', getBuyer);
 server.del('/buyer/services/buyers/:id', deleteBuyer);
 
-server.get('/', function root(request, response, next) {
+server.get('/', function root(req, res, next) {
     var routes = [
         'GET    /',
         'POST   /buyer/services/buyers',
@@ -100,10 +102,10 @@ server.get('/', function root(request, response, next) {
         'GET    /buyer/services/buyers/:id',
         'DELETE /buyer/services/buyers/:id'
     ];
-    response.send(200, routes);
+    res.send(200, routes);
     return next();
 });
 
-server.listen(8888, function() {
+server.listen(8888, 'pfitzgerald-pro', function() {
     console.log('%s listening at %s', server.name, server.url);
 });
